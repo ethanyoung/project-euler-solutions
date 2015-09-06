b551d13c030621e7beb1e096e738d5a5d29a2a97
Ethan Yang <ethan.y@outlook.com>
2015-09-05 20:52:06 -0700

    No need for array of numbers

--- p001.rb
+++ p001.rb
@@ -1,11 +1,8 @@
 n = 10
 
-numbers = []
+sum = 0
 (1 .. n - 1).each do |i|
-  numbers << i if i % 3 == 0 || i % 5 == 0
+  sum += i if i % 3 == 0 || i % 5 == 0
 end
 
-sum = numbers.inject :+
-
-p numbers
 p sum
