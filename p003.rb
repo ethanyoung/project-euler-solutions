a7a7e408df37e2e5353793d6e74ea4183ca2b59c
Ethan Yang <ethan.y@outlook.com>
2015-09-06 04:14:52 -0700

    p003.rb

--- p003.rb
+++ p003.rb
@@ -1,17 +1,14 @@
-include Math
+require 'prime'
+
 n = 600851475143
 
-def prime?(n)
-  (2 .. sqrt(n).floor).each do |i|
-    return false if n % i == 0
-  end
-  true
-end
-
 largest_prime_factor = -1
-
 (2 .. n).each do |i|
-  largest_prime_factor = i if n % i == 0 && prime?(i) && i > largest_prime_factor
+  if n % i == 0 && i.prime?
+    largest_prime_factor = i
+    n /= i
+    break if n == 1
+  end
 end
 
 p largest_prime_factor
