e2c0eb9f53ed28288939c71b90e1de05b7ca6006
Ethan Yang <ethan.y@outlook.com>
2015-09-09 04:16:09 -0700

    p005

---
+++ p005.rb
@@ -0,0 +1,15 @@
+n = 20
+
+def all_devided?(i, n)
+  [20, 19, 18, 17, 16, 15, 14, 13, 12, 11].each do |j|
+    return false unless i % j == 0
+  end
+  true
+end
+
+i = n
+until all_devided?(i, n) do
+  i += n
+end
+
+p i
